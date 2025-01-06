ef1c84cc487fb4c6ed9fde8bbd45271815c09214
Juan Carlos <juancarlosti@rede.ulbra.br>
2025-01-06 17:13:41 -0300

    configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-const http = require("http");
+import express from "express";
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/plain");
- 
-  if (req.url === "/") {
-    res.statusCode = 200;
-    res.end("Home page");
-  } else if (req.url === "/sobre") {
-    res.statusCode = 200;
-    res.end("About page");
-  }
+const port = 3000;
+const app = express();
+
+app.get("/", (req,res) => {
+  res.send("Home page");
 });
 
-server.listen(3000, () => {
-  console.log("Servidor em execução em http://localhost:3000/");
+app.get("/movies", (req,res) => {
+  res.send("Listagem de filmes : ['Digê']");
+});
+
+app.listen(port, () => {
+  console.log(`Porta ${port} - http://localhost:3000`);
 });
